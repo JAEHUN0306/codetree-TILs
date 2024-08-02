7010d27f57c3295ffde54792f3f8fd4df671e768
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2024-08-02 11:29:20 +0900

    Title: 두 사람, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240802/두 사람/two-person.java
+++ 240802/두 사람/two-person.java
@@ -1,16 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A_age = sc.nextInt();
-        char A_gen = sc.next().charAt(0);
-        int B_age = sc.nextInt();
-        char B_gen = sc.next().charAt(0);
-
-        if ((A_age >= 19 && A_gen == 'M') || (B_age >= 19 && B_age == 'M')) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // 첫 번째 사람의 나이와 성별 입력 받기
+        int age1 = scanner.nextInt();
+        String gender1 = scanner.next();
+        
+        // 두 번째 사람의 나이와 성별 입력 받기
+        int age2 = scanner.nextInt();
+        String gender2 = scanner.next();
+        
+        // 조건 검사
+        if ((age1 >= 19 && gender1.equals("M")) || (age2 >= 19 && gender2.equals("M"))) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
+        
+        scanner.close();
     }
 }
