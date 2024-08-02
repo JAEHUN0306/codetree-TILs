b5c9446e1894215c09d1e77b27240782705574a0
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2024-08-02 11:12:18 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240802/두 사람/two-person.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A_age = sc.nextInt();
+        char A_gen = sc.next().charAt(0);
+        int B_age = sc.nextInt();
+        char B_gen = sc.next().charAt(0);
+
+        if (A_age >= 19 && A_gen == 'M' || B_age >= 19 && B_age == "M") {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
